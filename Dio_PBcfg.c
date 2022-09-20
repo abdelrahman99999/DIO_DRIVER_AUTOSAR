9a091971611413f6c53973a86731e51b5ae827f7
Abdelrahman Elsayed <60861577+abdelrahman99999@users.noreply.github.com>
2022-09-20 16:54:11 +0200

    Update Dio_PBcfg.c

--- Dio_PBcfg.c
+++ Dio_PBcfg.c
@@ -6,6 +6,7 @@
  *
  * Description: Post Build Configuration Source file for TM4C123GH6PM Microcontroller - Dio Driver
  *
+ * Author: Abdelrahman Elsayed
  ******************************************************************************/
 
 #include "Dio.h"
